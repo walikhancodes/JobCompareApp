0351b2bbff16e52d893b412c76a274933422b4a9
Jiakang Chen <jchen3115@gatech.edu>
2024-02-28 23:37:40 -0600

    Add exceptions and unit testings

--- GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterEditJobActivity.java
+++ GroupProject/JobCompare6300/app/src/main/java/edu/gatech/seclass/jobcompare6300/EnterEditJobActivity.java
@@ -107,6 +107,57 @@ public class EnterEditJobActivity extends AppCompatActivity {
             }
         }
 
+        if (!isInteger(costEditText.getText().toString())) {
+            costEditText.setError("Invalid Entry. Please enter an integer.");
+            error = true;
+        }
+        if (!isDouble(salaryEditText.getText().toString())) {
+            salaryEditText.setError("Invalid Entry. Please enter a number.");
+            error = true;
+        }
+        if (!isDouble(bonusEditText.getText().toString())) {
+            bonusEditText.setError("Invalid Entry. Please enter a number.");
+            error = true;
+        }
+        if (!isInteger(stockEditText.getText().toString())) {
+            stockEditText.setError("Invalid Entry. Please enter an integer.");
+            error = true;
+        }
+
+        try {
+            // Validation for fundEditText within 0-15
+            double fundValue = Double.parseDouble(fundEditText.getText().toString());
+            if (fundValue < 0 || fundValue > 15) {
+                fundEditText.setError("Out of range. Please enter a value between 0-15.");
+                error = true;
+            }
+        } catch (NumberFormatException e) {
+            fundEditText.setError("Invalid Entry. Please enter a number.");
+            error = true;
+        }
+        try {
+            // Validation for holidayEditText within 0-20
+            int holidayValue = Integer.parseInt(holidayEditText.getText().toString());
+            if (holidayValue < 0 || holidayValue > 20) {
+                holidayEditText.setError("Out of range. Please enter a value between 0-20.");
+                error = true;
+            }
+        } catch (NumberFormatException e) {
+            holidayEditText.setError("Invalid Entry. Please enter an integer.");
+            error = true;
+        }
+        try {
+            // Validation for stipendEditText within 0-75
+            double stipendValue = Double.parseDouble(stipendEditText.getText().toString());
+            if (stipendValue < 0 || stipendValue > 75) {
+                stipendEditText.setError("Out of range. Please enter a value between 0-75.");
+                error = true;
+            }
+        } catch (NumberFormatException e) {
+            stipendEditText.setError("Invalid Entry. Please enter a number.");
+            error = true;
+        }
+
         if (!error) {
             SQLiteManager sqLiteManager = SQLiteManager.instanceOfDatabase(this);
             String title = titleEditText.getText().toString();
@@ -143,5 +194,25 @@ public class EnterEditJobActivity extends AppCompatActivity {
         }
 
     }
+
+    private boolean isInteger(String input) {
+        try {
+            Integer.parseInt(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private boolean isDouble(String input) {
+        try {
+            Double.parseDouble(input);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
 
 }
