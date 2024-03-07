ebc71765513fdec87fad3205907f5eb7be7280a1
Jiakang Chen <jchen3115@gatech.edu>
2024-03-06 19:17:19 -0600

    Make some changes to test cases

--- GroupProject/JobCompare6300/app/src/androidTest/java/edu/gatech/seclass/jobcompare6300/UpdateListTest.java
+++ GroupProject/JobCompare6300/app/src/androidTest/java/edu/gatech/seclass/jobcompare6300/UpdateListTest.java
@@ -35,11 +35,11 @@ public class UpdateListTest {
     @Test
     public void updateJobOfferList1() {
         onView(withId(R.id.enterOfferBtn)).perform(click());
-        replaceTextHelper(R.id.titleEditText, "SDE");
-        replaceTextHelper(R.id.companyEditText, "comp1");
+        replaceTextHelper(R.id.titleEditText, "Engineer II");
+        replaceTextHelper(R.id.companyEditText, "comp4");
         replaceTextHelper(R.id.locationEditText, "SF");
         replaceTextHelper(R.id.costEditText, "100");
-        replaceTextHelper(R.id.salaryEditText, "10000");
+        replaceTextHelper(R.id.salaryEditText, "200000");
         replaceTextHelper(R.id.bonusEditText, "1000");
         replaceTextHelper(R.id.stockEditText, "1000");
         replaceTextHelper(R.id.fundEditText, "10");
@@ -52,12 +52,12 @@ public class UpdateListTest {
         onData(anything())
                 .inAdapterView(withId(R.id.jobsListView))
                 .atPosition(0)
-                .onChildView(withId(R.id.cellTitle)).check(matches(withText("SDE")));
+                .onChildView(withId(R.id.cellTitle)).check(matches(withText("Engineer II")));
 
         onData(anything())
                 .inAdapterView(withId(R.id.jobsListView))
                 .atPosition(0)
-                .onChildView(withId(R.id.cellCompany)).check(matches(withText("comp1")));
+                .onChildView(withId(R.id.cellCompany)).check(matches(withText("comp4")));
 
         onView(withId(R.id.backBtn)).perform(click());
     }
@@ -66,11 +66,11 @@ public class UpdateListTest {
     @Test
     public void updateJobOfferList2() {
         onView(withId(R.id.enterEditJobBtn)).perform(click());
-        replaceTextHelper(R.id.titleEditText, "SDE");
-        replaceTextHelper(R.id.companyEditText, "comp1");
+        replaceTextHelper(R.id.titleEditText, "Engineer VI");
+        replaceTextHelper(R.id.companyEditText, "comp5");
         replaceTextHelper(R.id.locationEditText, "SF");
         replaceTextHelper(R.id.costEditText, "100");
-        replaceTextHelper(R.id.salaryEditText, "10000");
+        replaceTextHelper(R.id.salaryEditText, "50000");
         replaceTextHelper(R.id.bonusEditText, "1000");
         replaceTextHelper(R.id.stockEditText, "1000");
         replaceTextHelper(R.id.fundEditText, "10");
@@ -84,12 +84,12 @@ public class UpdateListTest {
         onData(anything())
                 .inAdapterView(withId(R.id.jobsListView))
                 .atPosition(0)
-                .onChildView(withId(R.id.cellTitle)).check(matches(withText("SDE")));
+                .onChildView(withId(R.id.cellTitle)).check(matches(withText("Engineer VI")));
 
         onData(anything())
                 .inAdapterView(withId(R.id.jobsListView))
                 .atPosition(0)
-                .onChildView(withId(R.id.cellCompany)).check(matches(withText("comp1")));
+                .onChildView(withId(R.id.cellCompany)).check(matches(withText("comp5")));
 
         onView(withId(R.id.backBtn)).perform(click());
     }
@@ -99,8 +99,8 @@ public class UpdateListTest {
     @Test
     public void updateJobOfferList3() {
         onView(withId(R.id.enterEditJobBtn)).perform(click());
-        replaceTextHelper(R.id.titleEditText, "SDE");
-        replaceTextHelper(R.id.companyEditText, "comp1");
+        replaceTextHelper(R.id.titleEditText, "Sales");
+        replaceTextHelper(R.id.companyEditText, "comp7");
         replaceTextHelper(R.id.locationEditText, "SF");
         replaceTextHelper(R.id.costEditText, "100");
         replaceTextHelper(R.id.salaryEditText, "10000");
@@ -112,11 +112,11 @@ public class UpdateListTest {
         onView(withId(R.id.button)).perform(click());
 
         onView(withId(R.id.enterOfferBtn)).perform(click());
-        replaceTextHelper(R.id.titleEditText, "SWE");
-        replaceTextHelper(R.id.companyEditText, "comp2");
+        replaceTextHelper(R.id.titleEditText, "Sales II");
+        replaceTextHelper(R.id.companyEditText, "comp8");
         replaceTextHelper(R.id.locationEditText, "LA");
         replaceTextHelper(R.id.costEditText, "120");
-        replaceTextHelper(R.id.salaryEditText, "11000");
+        replaceTextHelper(R.id.salaryEditText, "16000");
         replaceTextHelper(R.id.bonusEditText, "1100");
         replaceTextHelper(R.id.stockEditText, "1100");
         replaceTextHelper(R.id.fundEditText, "11");
@@ -125,8 +125,8 @@ public class UpdateListTest {
         onView(withId(R.id.button)).perform(click());
 
         onView(withId(R.id.enterOfferBtn)).perform(click());
-        replaceTextHelper(R.id.titleEditText, "Engineer");
-        replaceTextHelper(R.id.companyEditText, "comp3");
+        replaceTextHelper(R.id.titleEditText, "Sales III");
+        replaceTextHelper(R.id.companyEditText, "comp9");
         replaceTextHelper(R.id.locationEditText, "Austin");
         replaceTextHelper(R.id.costEditText, "1000");
         replaceTextHelper(R.id.salaryEditText, "50000");
@@ -147,7 +147,7 @@ public class UpdateListTest {
         onData(anything())
                 .inAdapterView(withId(R.id.jobsListView))
                 .atPosition(3)
-                .onChildView(withId(R.id.cellTitle)).check(matches(withText("SDE")));
+                .onChildView(withId(R.id.cellTitle)).check(matches(withText("Sales")));
 
     }
 }
